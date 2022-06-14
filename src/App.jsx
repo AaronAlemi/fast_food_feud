cef89466428c533d764c28b834757083072e6911
AaronAlemi <alemiaaron@gmail.com>
2022-06-14 19:45:46 -0400

    final-version

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import * as React from "react"
 // IMPORT ANY NEEDED COMPONENTS HERE
 import { createDataSet } from "./data/dataset"
 import "./App.css"
+import Header from "./components/Header/Header"
+import Instructions from "./components/Instructions/Instructions"
+import Chip from "./components/Chip/Chip"
+import { useState } from "react"
+import { nutritionFacts } from "./constants"
+import NutritionalLabel from "./components/NutritionalLabel/NutritionalLabel"
 
 // don't move this!
 export const appInfo = {
@@ -20,7 +26,83 @@ export const appInfo = {
 // or this!
 const { data, categories, restaurants } = createDataSet()
 
+let categorySelected = false;
+let restaurantSelected = false;
+let menuItemSelected = false;
+
 export function App() {
+
+  let instructionContent = appInfo.instructions.start;
+
+
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [selectedRestaurant, setSelectedRestaurant] = useState(null);
+  const [selectedMenuItem, setSelectedMenuItem] = useState(null);
+
+  function categorySelectHandler(parameter) {
+    categorySelected = true;
+    setSelectedMenuItem(null);
+    menuItemSelected = false;
+
+    return setSelectedCategory(parameter);
+  }
+
+  function restaurantSelectHandler(parameter) {
+    restaurantSelected = true;
+    setSelectedMenuItem(null);
+    menuItemSelected = false;
+
+    return setSelectedRestaurant(parameter);
+  }
+
+  function menuItemSelectHandler(parameter) {
+    menuItemSelected = true;
+    return setSelectedMenuItem(parameter);
+  }
+
+  function categoryCloseHandler() {
+    setSelectedCategory(null);
+    //categorySelected = false;
+  }
+
+  function restaurantCloseHandler() {
+    setSelectedRestaurant(null);
+    //restaurantSelected = false;
+  }
+
+  function menuItemCloseHandler() {
+    setSelectedMenuItem(null);
+    //menuItemSelected = false;
+  }
+
+
+/*
+  if (!categorySelected && !restaurantSelected) {
+    instructionContent = appInfo.instructions.start;
+    console.log(instructionContent)
+  }
+  else if (categorySelected && !restaurantSelected) {
+    instructionContent = appInfo.instructions.onlyCategory;
+    console.log(instructionContent)
+  }
+  else if (!categorySelected && restaurantSelected) {
+    instructionContent = appInfo.instructions.onlyRestaurant;
+    console.log(instructionContent)
+  }
+  else if (!menuItemSelected) {
+    instructionContent = appInfo.instructions.noSelectedItem;
+    console.log(instructionContent)
+  }
+  else {
+    instructionContent = appInfo.instructions.allSelected;
+    console.log(instructionContent)
+  }
+*/
+  
+let currentMenuItems = data.filter((menuItem) => {
+  return menuItem.food_category == selectedCategory && menuItem.restaurant == selectedRestaurant;
+})
+
   return (
     <main className="App">
       {/* CATEGORIES COLUMN */}
@@ -28,30 +110,80 @@ export function App() {
         <div className="categories options">
           <h2 className="title">Categories</h2>
           {/* YOUR CODE HERE */}
+          
+            {categories.map((item, index) => (
+              <Chip key={index} label={item} isActive={selectedCategory === item} onClick = { () => categorySelectHandler(item) } onClose={() => categoryCloseHandler()} ></Chip>
+            ))}
+         
+          
         </div>
       </div>
 
       {/* MAIN COLUMN */}
       <div className="container">
         {/* HEADER GOES HERE */}
+        <Header title={appInfo.title} tagline={appInfo.tagline} description={appInfo.description}/>
 
         {/* RESTAURANTS ROW */}
         <div className="RestaurantsRow">
           <h2 className="title">Restaurants</h2>
-          <div className="restaurants options">{/* YOUR CODE HERE */}</div>
+          <div className="restaurants options">
+            {/* YOUR CODE HERE */}
+            {restaurants.map((rest, index) => (
+              <Chip key={index} label={rest} isActive={selectedRestaurant === rest} onClick = { () => restaurantSelectHandler(rest)} onClose={() => restaurantCloseHandler()} ></Chip>
+            ))}
+            
+            </div>
         </div>
 
         {/* INSTRUCTIONS GO HERE */}
+        {/*
+          <Instructions instructions={appInfo.instructions.start}/>
+          
+            */}
+
+
+          {(
+            (!selectedCategory && !selectedRestaurant && !selectedMenuItem && <Instructions instructions={appInfo.instructions.start}/>)
+            ||
+            (selectedCategory && !selectedRestaurant && !selectedMenuItem && <Instructions instructions={appInfo.instructions.onlyCategory}/>)
+            ||
+            (!selectedCategory && selectedRestaurant && !selectedMenuItem && <Instructions instructions={appInfo.instructions.onlyRestaurant}/>)
+            ||
+            (selectedCategory && selectedRestaurant && !selectedMenuItem && <Instructions instructions={appInfo.instructions.noSelectedItem}/>)
+            ||
+            (selectedCategory && selectedRestaurant && selectedMenuItem && <Instructions instructions={appInfo.instructions.allSelected}/>)
+          )}
+
+           {/*(
+            (!categorySelected && !restaurantSelected && !menuItemSelected && <Instructions instructions={appInfo.instructions.start}/>)
+            ||
+            (categorySelected && !restaurantSelected && !menuItemSelected && <Instructions instructions={appInfo.instructions.onlyCategory}/>)
+            ||
+            (!categorySelected && restaurantSelected && !menuItemSelected && <Instructions instructions={appInfo.instructions.onlyRestaurant}/>)
+            ||
+            (categorySelected && restaurantSelected && !menuItemSelected && <Instructions instructions={appInfo.instructions.noSelectedItem}/>)
+            ||
+            (categorySelected && restaurantSelected && menuItemSelected && <Instructions instructions={appInfo.instructions.allSelected}/>)
+           )*/}
+  
 
         {/* MENU DISPLAY */}
         <div className="MenuDisplay display">
           <div className="MenuItemButtons menu-items">
             <h2 className="title">Menu Items</h2>
             {/* YOUR CODE HERE */}
+            {
+            currentMenuItems.map((menuItem, index) => {
+              return (<Chip key={index} label={menuItem.item_name} isActive={selectedMenuItem == menuItem} onClick = { () => menuItemSelectHandler(menuItem)} onClose={() => menuItemCloseHandler()} />)
+          })}
           </div>
 
           {/* NUTRITION FACTS */}
-          <div className="NutritionFacts nutrition-facts">{/* YOUR CODE HERE */}</div>
+          <div className="NutritionFacts nutrition-facts">
+            {/* YOUR CODE HERE */}
+            {selectedMenuItem ? <NutritionalLabel item={selectedMenuItem}/> : <></> }
+          </div>
         </div>
 
         <div className="data-sources">
--- src/components/Chip/Chip.jsx
+++ src/components/Chip/Chip.jsx
@@ -1,11 +1,21 @@
 import * as React from "react"
 import "./Chip.css"
+import { useState } from "react"
 
-export function Chip({ label = "", isActive = false }) {
+export function Chip({ label = "", isActive = false, onClick = () => {}, onClose = () => {} }) {
+  let buttonClassName;
+  if (isActive == false) {
+    buttonClassName = "chip"
+  }
+  else {
+    buttonClassName = "chip active"
+  }
+
+  
   return (
-    <button className="chip">
-      <p className="label"></p>
-      <span className="close" role="button">{`X`}</span>
+    <button onClick={onClick} className={buttonClassName}>
+      <p className="label">{label}</p>
+      <span className="close" onClick={event => {onClose(); event.stopPropagation()} } role="button">{`X`}</span>
     </button>
   )
 }
